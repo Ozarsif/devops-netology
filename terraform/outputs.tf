2e34bd460caa7a6d30363c6b545eecb2f2a4380c
Ozarsif <67621467+Ozarsif@users.noreply.github.com>
2021-06-13 14:53:54 +0600

    adding tf files for 07-terraform-02-syntax

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "private_ip" {
+	value = "172.31.25.68"
+}
+
+outout "network_id" {
+	value = "subnet-386be740"
+}
+
+output "aws_acc_id" {
+	value = "Ozarsif"
+}
+
+output "aws_user_id" {
+	value = "netology"
+}
+
+output "aws_region" {
+	value = "us-west-2"
+}
